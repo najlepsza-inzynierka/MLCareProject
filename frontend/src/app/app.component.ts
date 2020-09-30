539598f34ba00c49a1cfa3f7fd43bd3b2a2a9d26
magk98 <magk98@poczta.fm>
2020-09-30 21:14:53 +0200

    added logout buttons - todo visibility

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {Observable} from 'rxjs';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {map, shareReplay} from 'rxjs/operators';
+import {AdminAuthService} from './services/admin-auth.service';
+import {Router} from '@angular/router';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +18,19 @@ export class AppComponent {
       map(result => result.matches),
       shareReplay()
     );
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+              private adminAuthService: AdminAuthService,
+              private authService: AuthService,
+              private router: Router) {}
+
+  signOutAdmin(){
+    this.adminAuthService.logout();
+    this.router.navigateByUrl(`/`);
+  }
+
+  signOutUser(){
+    this.authService.logout();
+    this.router.navigateByUrl(`/`);
+  }
 
 }
